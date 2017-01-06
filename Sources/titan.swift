3171d6508d24e3a1c0f7a03da6539a792d7fb243
Swizzlr <git@swizzlr.co>
2017-01-06 12:23:06 +0000

    Iteration 0 (failing tests)

--- Sources/titan.swift
+++ Sources/titan.swift
@@ -1,29 +1,90 @@
 public struct Request {
-  public init(_ method: String, _ path: String) {
+  public init(_ method: String, _ path: String, _ body: String = "") {
     self.method = method
     self.path = path
+    self.body = body
   }
-  let method: String
-  let path: String
+  public let method: String
+  public let path: String
+  public let body: String
 }
-public struct Response { }
-public final class Titan {
-  private var middlewareStack = Array<() -> String>()
-  public func get(path: String, handler: @escaping () -> String) {
-    middlewareStack.append(handler)
-  }
-  public func app(request: Request) -> String {
-    return middlewareStack.first?() ?? "404"
-  }
-}
-fileprivate let GlobalDefaultTitanInstance = Titan()
-public let TitanApp = GlobalDefaultTitanInstance.app
 
-public func get(path: String, handler: @escaping () -> String) {
-  GlobalDefaultTitanInstance.get(path: path, handler: handler)
+public protocol RequestType {
+  var body: String { get }
+  var path: String { get }
+  var method: String { get }
+}
+
+public protocol ResponseType {
+  var body: String { get }
+}
+
+public struct Response {
+  let code: Int
+  public let body: String
+  let encoding: Encoding = .utf8
+  enum Encoding {
+    case utf8, utf16
+  }
+}
+
+public func TitanAppReset() {
+  GlobalDefaultTitanInstance = Titan()
+  TitanApp = GlobalDefaultTitanInstance.app
+}
+
+extension Response: ResponseType {}
+
+extension Request: RequestType {}
+
+public typealias Middleware = (RequestType, ResponseType) -> (RequestType, ResponseType)
+public final class Titan {
+  private var middlewareStack = Array<Middleware>()
+  public func get(path: String, handler: @escaping Middleware) {
+    route(method: "GET", path: path, handler: handler)
+  }
+
+  func route(method: String, path: String, handler: @escaping Middleware) {
+    let routeWare: Middleware = { (req, res) in
+      guard req.path == path && req.method == method else {
+        return (req, Response(code: 404, body: ""))
+      }
+      return handler(req, res)
+    }
+    middlewareStack.append(routeWare)
+  }
+
+  public func app(request: Request) -> ResponseType {
+    typealias Result = (RequestType, ResponseType)
+
+    let initialReq = Request("", "")
+    let initialRes = Response(code: 0, body: "")
+    let initial: Result = (initialReq, initialRes)
+    let res = middlewareStack.reduce(initial) { (res, next) -> Result in
+      return next(res.0, res.1)
+    }
+    return res.1
+  }
+}
+
+fileprivate var GlobalDefaultTitanInstance = Titan()
+public private(set) var TitanApp = GlobalDefaultTitanInstance.app
+
+public func get(path: String, handler: @escaping (RequestType) -> String) {
+  GlobalDefaultTitanInstance.get(path: path, handler: toMiddleware(handler))
+}
+
+private func toMiddleware(_ handler: @escaping (RequestType) -> String) -> Middleware {
+  return { req, res in
+    return (req, Response(code: 200, body: handler(req)))
+  }
+}
+
+public func get(_ path: String, handler: @escaping (RequestType) -> String) {
+  get(path: path, handler: handler)
 }
 
 public func get(_ path: String, handler: @escaping () -> String) {
-  get(path: path, handler: handler)
+  get(path: path, handler: { _ in handler() } )
 }
 
--- examples/Examples.swift
+++ examples/Examples.swift
@@ -1,4 +1,4 @@
-get('username') { req in
+get("username") { req in
   return "swizzlr"
 }
 
@@ -16,40 +16,40 @@ func unsafeDB() {
   return Connections.lookupConnForThread(Thread.current) ?? UnsafePostgres()
 }
 
-middleware('*') { _ in
+middleware("*") { _ in
   start = Date()
 }
 
-middleware('*') { req in
+middleware("*") { req in
   log(req)
   return
 }
 
-middleware('/user/**/') {
-  guard req['Authentication'] == "password" else {
+middleware("/user/*") {
+  guard req["Authentication"] == "password" else {
     throw .NotAuthorized
   }
 }
 
 // Add cache control headers
-middleware('*') { req in
+middleware("*") { req in
 
 }
 
-get('/username') { _ in
+get("/username") { _ in
   return "swizzlr"
 }
 
-get('*') { _ in
+get("*") { _ in
   return 404
 }
 
-middleware('*') { _ in
+middleware("*") { _ in
   log(Date() - start)
 }
 
 middleware() { req, res in
-  req.headers['X-Translated-By'] = 'My custom middleware'
+  req.headers["X-Translated-By"] = "My custom middleware"
   req.body = MessagePack.decode(req.body)
   return req // res transparently inserted
 }
