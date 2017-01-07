e97b0cea3fcba6869cb1acdbde8675578b8cd722
Swizzlr <git@swizzlr.co>
2017-01-07 23:39:35 +0000

    Kill some warnings.

--- Sources/TitanTopLevelSugar.generated.swift
+++ Sources/TitanTopLevelSugar.generated.swift
@@ -2,51 +2,51 @@
 // DO NOT EDIT
 
 
-public func get(path path: String, handler handler: @escaping Middleware) {
+public func get(path: String, handler: @escaping Middleware) {
   GlobalDefaultTitanInstance.get(path: path, handler: handler)
 }
 
-public func post(path path: String, handler handler: @escaping Middleware) {
+public func post(path: String, handler: @escaping Middleware) {
   GlobalDefaultTitanInstance.post(path: path, handler: handler)
 }
 
-public func put(path path: String, handler handler: @escaping Middleware) {
+public func put(path: String, handler: @escaping Middleware) {
   GlobalDefaultTitanInstance.put(path: path, handler: handler)
 }
 
-public func patch(path path: String, handler handler: @escaping Middleware) {
+public func patch(path: String, handler: @escaping Middleware) {
   GlobalDefaultTitanInstance.patch(path: path, handler: handler)
 }
 
-public func delete(path path: String, handler handler: @escaping Middleware) {
+public func delete(path: String, handler: @escaping Middleware) {
   GlobalDefaultTitanInstance.delete(path: path, handler: handler)
 }
 
-public func options(path path: String, handler handler: @escaping Middleware) {
+public func options(path: String, handler: @escaping Middleware) {
   GlobalDefaultTitanInstance.options(path: path, handler: handler)
 }
 
-public func head(path path: String, handler handler: @escaping Middleware) {
+public func head(path: String, handler: @escaping Middleware) {
   GlobalDefaultTitanInstance.head(path: path, handler: handler)
 }
 
-public func middleware(_ path: String, handler handler: @escaping Middleware) {
+public func middleware(path: String, handler: @escaping Middleware) {
   GlobalDefaultTitanInstance.middleware(path, handler: handler)
 }
 
-public func get(_ path: String, handler handler: @escaping (RequestType) -> String) {
+public func get(path: String, handler: @escaping (RequestType) -> String) {
   GlobalDefaultTitanInstance.get(path, handler: handler)
 }
 
-public func get(_ path: String, handler handler: @escaping () -> String) {
+public func get(path: String, handler: @escaping () -> String) {
   GlobalDefaultTitanInstance.get(path, handler: handler)
 }
 
-public func middleware(_ path: String, handler handler: @escaping () -> ()) {
+public func middleware(path: String, handler: @escaping () -> ()) {
   GlobalDefaultTitanInstance.middleware(path, handler: handler)
 }
 
-public func route(method method: String?, path path: String, handler handler: @escaping Middleware) {
+public func route(method: String?, path: String, handler: @escaping Middleware) {
   GlobalDefaultTitanInstance.route(method: method, path: path, handler: handler)
 }
 
