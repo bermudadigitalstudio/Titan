03b01ddabdfe4d0fc6abf3040eb3200c052c7804
Swizzlr <git@swizzlr.co>
2017-01-09 11:11:13 +0000

    - Pin Sourcery to v0.5.0 - Add script/generate.sh which generates code for us in a pipeline, taking the guesswork out of generating http method overloads and syntactic sugar - Explicitly mark generated files with generated filename - Rename instance convenience routes to instance routes by method – we have more conveniences coming later!

--- Sources/Titan.swift
+++ Sources/Titan.swift
@@ -1,19 +1,8 @@
 @_exported import TitanCore
 
-enum HTTPMethod {
-  case GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD
-}
 internal var GlobalDefaultTitanInstance = Titan()
 
 public private(set) var TitanApp = GlobalDefaultTitanInstance.app
-
-//public func middleware(_ path: String, handler: @escaping Middleware) {
-//  GlobalDefaultTitanInstance.middleware(path, handler: handler)
-//}
-//
-//public func get(path: String, handler: @escaping (RequestType) -> String) {
-//  GlobalDefaultTitanInstance.get(path: path, handler: toMiddleware(handler))
-//}
 
 public func TitanAppReset() {
   GlobalDefaultTitanInstance = Titan()
