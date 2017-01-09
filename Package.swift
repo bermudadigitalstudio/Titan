fe056a9c8a6d23579d67e8625fb151ad3ac1632e
Swizzlr <git@swizzlr.co>
2017-01-09 16:19:59 +0000

    Restructure package.swift and create server delegate.

--- Package.swift
+++ Package.swift
@@ -3,15 +3,26 @@ import PackageDescription
 var package = Package(
   name: "Titan",
   dependencies: [
-    .Package(url: "https://github.com/bermudadigitalstudio/titan-core.git", majorVersion: 0),
-//    .Package(url: "https://github.com/krzysztofzablocki/Sourcery.git", "0.5.0")
+    .Package(url: "https://github.com/bermudadigitalstudio/titan-core.git", majorVersion: 0)
   ],
-  exclude: ["script", "Templates", "examples", "benchmark"]
+  exclude: ["script", "Templates", "examples", "benchmark", "TitanServerDelegate"]
 )
 
+// Uncomment the following to develop TitanServerDelegate
+package.exclude = package.exclude.filter { $0 != "TitanServerDelegate" }
+
+package.dependencies.append(
+ .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1)
+)
+
+// Uncomment the following to install sourcery for development
+//package.dependencies.append(
+//  .Package(url: "https://github.com/krzysztofzablocki/Sourcery.git", "0.5.0")
+//)
+
 // Uncomment the following to compile the benchmark
-//package.exclude.removeLast()
+//package.exclude = package.exclude.filter { $0 != "benchmark" }
 //
 //package.targets = [
-// Target(name: "benchmark", dependencies: ["Titan"])
+//  Target(name: "benchmark", dependencies: ["Titan"])
 //]
