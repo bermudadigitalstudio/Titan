299a09eb3f80c15dd580058a7c2b1f3d7211b141
Swizzlr <git@swizzlr.co>
2017-01-09 15:26:46 +0000

    Create benchmark suite.

--- Package.swift
+++ Package.swift
@@ -1,9 +1,17 @@
 import PackageDescription
 
-let package = Package(
+var package = Package(
   name: "Titan",
   dependencies: [
     .Package(url: "https://github.com/bermudadigitalstudio/titan-core.git", majorVersion: 0),
 //    .Package(url: "https://github.com/krzysztofzablocki/Sourcery.git", "0.5.0")
-  ]
+  ],
+  exclude: ["script", "Templates", "examples", "benchmark"]
 )
+
+// Uncomment the following to compile the benchmark
+//package.exclude.removeLast()
+//
+//package.targets = [
+// Target(name: "benchmark", dependencies: ["Titan"])
+//]
