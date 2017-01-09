bd3e311f7879ac057d85d728a03f437dce9f6959
Swizzlr <git@swizzlr.co>
2017-01-09 15:03:24 +0000

    Fix Package.swift.

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
   name: "Titan",
   dependencies: [
     .Package(url: "https://github.com/bermudadigitalstudio/titan-core.git", majorVersion: 0),
-//    .Package(url: "https://github.com/krzysztofzablocki/Sourcery.git", version: "0.5.0")
+//    .Package(url: "https://github.com/krzysztofzablocki/Sourcery.git", "0.5.0")
   ]
 )
