33da5e3c19b24440ccddb4242f8f13f1446ca738
Swizzlr <git@swizzlr.co>
2017-01-09 14:32:42 +0000

    Return of the overloads!

---
+++ Sources/MiddlewareOverloads.swift
@@ -0,0 +1,31 @@
+extension Titan {
+  private func toMiddleware(_ handler: @escaping () -> String) -> Middleware {
+    return { req, res in
+      return (req, Response(200, handler()))
+    }
+  }
+  private func toMiddleware(_ handler: @escaping (RequestType) -> String) -> Middleware {
+    return { req, res in
+      return (req, Response(200, handler(req)))
+    }
+  }
+
+  private func toMiddleware(_ handler: @escaping () -> ()) -> Middleware {
+    return { req, res in
+      handler()
+      return (req, res)
+    }
+  }
+
+  public func get(_ path: String, handler: @escaping (RequestType) -> String) {
+    get(path: path, handler: toMiddleware(handler))
+  }
+
+  public func get(_ path: String, handler: @escaping () -> String) {
+    get(path: path, handler: toMiddleware(handler))
+  }
+
+//  public func middleware(_ path: String, handler: @escaping () -> ()) {
+//    middleware(path, handler: toMiddleware(handler))
+//  }
+}
