ab111e8c792981eff18d657a3313a3602c7b3e4c
Swizzlr <git@swizzlr.co>
2017-01-12 13:50:01 +0000

    Update dependency URLs.

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
   name: "Titan",
   dependencies: [
-    .Package(url: "https://github.com/bermudadigitalstudio/titan-core.git", majorVersion: 0),
-    .Package(url: "https://github.com/bermudadigitalstudio/titan-router.git", majorVersion: 0, minor: 0),
+    .Package(url: "https://github.com/bermudadigitalstudio/TitanCore.git", majorVersion: 0, minor: 1),
+    .Package(url: "https://github.com/bermudadigitalstudio/TitanRouter.git", majorVersion: 0, minor: 0),
     .Package(url: "https://github.com/bermudadigitalstudio/TitanTopLevel.git", majorVersion: 0, minor: 0),
   ]
 )
