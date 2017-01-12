c151648211f79f9e47f088c315127fe2d02f0594
Swizzlr <git@swizzlr.co>
2017-01-12 10:47:48 +0000

    Refactor top level code into separate module.

--- Package.swift
+++ Package.swift
@@ -1,14 +1,10 @@
 import PackageDescription
 
-var package = Package(
+let package = Package(
   name: "Titan",
   dependencies: [
     .Package(url: "https://github.com/bermudadigitalstudio/titan-core.git", majorVersion: 0),
-    .Package(url: "https://github.com/bermudadigitalstudio/titan-router.git", majorVersion: 0, minor: 0)
+    .Package(url: "https://github.com/bermudadigitalstudio/titan-router.git", majorVersion: 0, minor: 0),
+    .Package(url: "https://github.com/bermudadigitalstudio/TitanTopLevel.git", majorVersion: 0, minor: 0),
   ]
 )
-
-// Uncomment the following to install sourcery for development
-//package.dependencies.append(
-//  .Package(url: "https://github.com/krzysztofzablocki/Sourcery.git", "0.5.0")
-//)
--- Sources/Titan.swift
+++ Sources/Titan.swift
@@ -1,11 +1,3 @@
 @_exported import TitanCore
 @_exported import TitanRouter
-
-internal var GlobalDefaultTitanInstance = Titan()
-
-public private(set) var TitanApp = GlobalDefaultTitanInstance.app
-
-public func TitanAppReset() {
-  GlobalDefaultTitanInstance = Titan()
-  TitanApp = GlobalDefaultTitanInstance.app
-}
+@_exported import TitanTopLevel
