6d6008d51868f5e66b767e9d26a486d9eaa4b619
Swizzlr <git@swizzlr.co>
2017-01-17 14:37:44 +0000

    Update Titan router and remove prototyped mutable param code.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
   name: "Titan",
   dependencies: [
     .Package(url: "https://github.com/bermudadigitalstudio/TitanCore.git", majorVersion: 0, minor: 2),
-    .Package(url: "https://github.com/bermudadigitalstudio/TitanRouter.git", majorVersion: 0, minor: 0),
-    .Package(url: "https://github.com/bermudadigitalstudio/TitanTopLevel.git", majorVersion: 0, minor: 0),
+    .Package(url: "https://github.com/bermudadigitalstudio/TitanRouter.git", majorVersion: 0, minor: 1),
+    .Package(url: "https://github.com/bermudadigitalstudio/TitanTopLevel.git", majorVersion: 0, minor: 1),
   ]
 )
