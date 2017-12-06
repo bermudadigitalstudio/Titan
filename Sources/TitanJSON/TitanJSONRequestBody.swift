2754c4ee7eee130650538fa2c62ff96c9a315062
Thomas Catterall <me@swizzlr.co>
2017-12-06 09:33:05 -0500

    Whitespace fix.

--- Sources/TitanJSON/TitanJSONRequestBody.swift
+++ Sources/TitanJSON/TitanJSONRequestBody.swift
@@ -2,7 +2,6 @@ import TitanCore
 import Foundation
 
 public extension RequestType {
-
     /// Return the body content as a JSON object, if possible, otherwise return nil.
     /// Uses `Foundation` JSONSerialization for decoding.
     var json: Any? {
