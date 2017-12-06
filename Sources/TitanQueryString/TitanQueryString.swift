2bedde6fcbc79085ee0021a6cad23fe61206c74f
Thomas Catterall <me@swizzlr.co>
2017-12-06 09:13:11 -0500

    Document logic.

--- Sources/TitanQueryString/TitanQueryString.swift
+++ Sources/TitanQueryString/TitanQueryString.swift
@@ -2,17 +2,20 @@ import Foundation
 import TitanCore
 
 public extension RequestType {
-
+    /// The pairs of keys and values in the query string of the `RequestType`s path.
     public var queryPairs: [(key: String, value: String)] {
-
+        // Ensure there is a query string, otherwise return
         guard let indexOfQuery = self.path.index(of: "?") else {
             return []
         }
 
         let query = self.path.suffix(from: indexOfQuery).dropFirst()
+        // Create an array of the individual query pairs, e.g. ["foo=bar", "baz=qux"]
         let pairs = query.split(separator: "&")
 
+        // Decode `foo=bar` -> `(key: "foo", value: "bar")`, percent decoding any values along the way
         return pairs.map { pair -> (key: String, value: String) in
+            // Separate the query pair into an array, e.g. "foo=bar" -> ["foo", "bar"]
             let comps = pair.split(separator: "=").map { chars -> String in
                 return String(chars).removingPercentEncoding ?? ""
                 }.map {
